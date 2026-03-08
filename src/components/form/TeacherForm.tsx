cd014f1fb08ac4166126026f8dc8d4c35a0a8a83
Abdur Razzaq <devarcoder789@gmail.com>
2026-03-09 00:48:41 +0500

    schema created for form

--- src/components/form/TeacherForm.tsx
+++ src/components/form/TeacherForm.tsx
@@ -1,6 +1,36 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { email, z } from "zod"
+
+const schema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters long!" })
+    .max(20, { message: "Username must be at most 20 characters long!" }),
+  email: z.string().email({ message: "Invalid email address!" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long!" }),
+  firstName: z.string().min(1, { message: "First name is required!" }),
+  lastName: z.string().min(1, { message: "Last name is required!" }),
+  phone: z.string().min(1, { message: "Phone is required!" }),
+  address: z.string().min(1, { message: "Address is required!" }),
+  bloodType: z.string().min(1, { message: "Blood Type is required!" }),
+  birthday: z.date({ message: "Birthday is required!" }),
+  sex: z.enum(["male", "female"], { message: "Sex is required!" }),
+  img: z.instanceof(File, { message: "Image is required" }),
+});
 const TeacherForm = ({type, data}:{type: "create" | "update";
     data?: any;
 }) => {
+    const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
   return (
     <form action="">
         form
